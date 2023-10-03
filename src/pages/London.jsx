cb82206429eb802f06665a537b2ac2fe143ccfe3
wasif kareem <wasifkareem2762@gmail.com>
2023-10-03 07:47:56 +0530

    buttons fixed

--- src/pages/London.jsx
+++ src/pages/London.jsx
@@ -19,28 +19,32 @@ const London = () => {
           {londonData.slice(0, 6).map((item) => (
             <Card item={item} key={item.area} />
           ))}
-          <button
-            onClick={expandClick}
-            className=" px-3 py-2 bg-blue-800 text-white font-semibold rounded-2xl"
-          >
-            Show More
-          </button>
         </div>
       ) : (
         <div className="flex flex-wrap pb-4 justify-center gap-10 mt-3">
           {londonData.map((item) => (
             <Card item={item} />
           ))}
+        </div>
+      )}
+
+      <div className=" flex justify-center mb-4">
+        {toggle === false ? (
+          <button
+            onClick={expandClick}
+            className=" px-3 py-2 bg-blue-800  text-white font-semibold rounded-2xl"
+          >
+            Show More
+          </button>
+        ) : (
           <button
             onClick={hideClick}
             className=" px-3 py-2 bg-blue-800 text-white font-semibold rounded-2xl"
           >
             Show Less
           </button>
-        </div>
-      )}
-
-      <div></div>
+        )}
+      </div>
     </div>
   );
 };
--- src/pages/Mumbai.jsx
+++ src/pages/Mumbai.jsx
@@ -20,28 +20,32 @@ const Mumbai = () => {
           {mumbaiData.slice(0, 6).map((item) => (
             <Card item={item} key={item.area} />
           ))}
-          <button
-            onClick={expandClick}
-            className=" px-3 py-2 bg-blue-800 text-white font-semibold rounded-2xl"
-          >
-            Show More
-          </button>
         </div>
       ) : (
         <div className="flex flex-wrap pb-4 justify-center gap-10 mt-3">
           {mumbaiData.map((item) => (
             <Card item={item} />
           ))}
+        </div>
+      )}
+
+      <div className=" flex justify-center mb-4">
+        {toggle === false ? (
+          <button
+            onClick={expandClick}
+            className=" px-3 py-2 bg-blue-800  text-white font-semibold rounded-2xl"
+          >
+            Show More
+          </button>
+        ) : (
           <button
             onClick={hideClick}
             className=" px-3 py-2 bg-blue-800 text-white font-semibold rounded-2xl"
           >
             Show Less
           </button>
-        </div>
-      )}
-
-      <div></div>
+        )}
+      </div>
     </div>
   );
 };
